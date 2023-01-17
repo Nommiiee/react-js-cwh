b3e75fa40c00cafcba99715757e8ea17b979de24
Nommiiee <kunalkashyp3@gmail.com>
2023-01-17 15:47:43 +0530

    tailwind Configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/img/logo.svg";
+import "./assets/css/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="w-full flex items-center justify-center h-screen bg-gray-900 text-white">
+      <header className="w-6/12 h-5/6 text-center flex flex-col gap-4-center justify-center">
+        <img src={logo} id="logo" className="m-auto w-10/12" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
