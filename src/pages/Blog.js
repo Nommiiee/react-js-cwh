faf4101305f6a2ce73b23f5d53a6e6e72d0970ec
Kunal Kashyap <kunalkashyp3@gmail.com>
2023-01-27 17:38:03 +0530

    news app

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,56 +1,29 @@
-import React from "react";
+import React, { Component } from 'react'
 
-export default function Blog() {
+export default class Blog extends Component {
+  render() {
+    let {title, description, image, date, content} = this.props
+    console.log(title, description, image, date, content)
+    return (
+      <div className='w-full h-full flex item-center justify-center'>      
+        <div className='border-2 border-black  max-w-6xl flex items-center justify-center flex-wrap '>
+        <div className='w-11/12 md:w-6/12 lg:w-4/12 p-4 card'>
+            <div className='w-full h-full rounded-2xl shadow-2xl border-2 border-black overflow-hidden'>
+            <div className='w-full h-full overflow-hidden flex items-center justify-center'>
 
+            <img className='w-full h-40 object-cover' src={image} alt={title} />
+            </div>
+            <div className='p-4'>
 
-
-
-  const truncateString = (e) => {
-    console.log(e)
-      if(e.target.innerText.length > 200){
-        e.target.innerText = e.target.innerText.substring(0, 100) + "...";
-      } else{
-        return e.target.innerText;
-      }
-  };
-
-  return (
-    <>
-      <div className="w-full h-full flex items-center justify-center flex-col">
-        <div className="w-full h-full max-w-6xl overflow-x-hidden ">
-          <div className="mt-10">
-            <h1 className="text-center text-3xl font-serif font-semibold">
-              This is the start of the blog page.
-            </h1>
+            <h1 className='text-xl font-semibold'>{title}</h1>
+            <p className='mt-2 text-md'>{description}</p>
+            <p className='mt-2 text-sm'>{date}</p>
+            <p className='mt-2 text-lg'>{content}</p>
+            </div>
+            </div>
           </div>
-          <ul className="flex flex-col justify-center items-center gap-y-10">
-            <li className="w-full h-full shadow-xl rounded-xl cursor-pointer overflow-hidden">
-              <div className="w-full flex items-center p-4 ">
-                  <div className="w-full flex items-center gap-x-6">
-                      <div className="  w-48 justify-center flex items-center" >
-                        <img className=" rounded-full " alt="" src="https://randomuser.me/api/portraits/women/67.jpg" />
-                      </div>
-                      <div className=" w-full  ">
-                        <h1 className="text-2xl font-semibold">
-                          Hello world!
-                        </h1>
-                        <div className="text-md text-gray-500 font-semibold w-full ">
-                        <span className="" >
-                        Auther: John Doe, Post on: 12/12/2023 
-                        </span>
-                        </div>
-                        <p className="text-base max-h-24  max-w-max summary-container" onLoad={truncateString}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        </p>
-                      </div>  
-                  </div>
-              </div>
-            </li>
-            
-          </ul>
-
         </div>
       </div>
-    </>
-  );
+    )
+  }
 }
