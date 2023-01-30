13fc82cd41a821b5ac8c8c0398745be66d160ae8
Kunal Kashyap <kunalkashyp3@gmail.com>
2023-01-30 19:35:41 +0530

    broken component did mount

--- src/pages/News.js
+++ src/pages/News.js
@@ -3,28 +3,38 @@ import React, { Component } from "react";
 import NewsComponent from "./blogPost/NewsComponent";
 
 export default class Blog extends Component {
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     articles: [],
+  //     loading: false,
+  //     page: 1,
+  //   };
+  // };
 
-  async componentDidMount(){
-    const URL = `https://newsapi.org/v2/top-headlines?country=IN&apiKey=6c7a00a45b7149798b05cdcc0465a8c8&page=1`;
+  componentDidMount(){
+    console.log("Component Did Mount");
+  }
+  
+  
+  async loadNews(){
+    const URL = `https://newsapi.org/v2/top-headlines?country=IN&apiKey=6c7a00a45b7149798b05cdcc0465a8c8&page=${this.state.page}&pageSize=2`;
+
     const data = await fetch(URL,{
       method: "GET",
-    }).then((response) => response.json()).catch(err => console.log(err));
-    this.setState({
-      articles: data.articles,
-      // loading: true,
-    })
-  }
+    }).then((response) => response.json()).catch(err => console.log(err)).finally(() => console.log("Done"));
+    // push the data to the state
+    this.setState({ 
+      articles: [...this.state.articles, ...data.articles],
+      loading: false,
+      });
+      console.log(this.state.articles);
+    };
+
+
+ 
 
   render() {
-    
     return (
       <div className="w-full h-full flex item-center justify-center ">
         <div className=" w-full max-w-6xl">
@@ -35,10 +45,10 @@ export default class Blog extends Component {
           </div>
           <div className="w-full h-full flex justify-center items-center">
             <div className=" flex h-full flex-wrap w-full">
-              {this.state.articles.map((article) => {
+              {/* {this.state.articles.map((article) => {
                   return (
                     <NewsComponent
-                    key={article.url}
+                      key={Math.random()}
                       title={article.title?article.title.slice(0,30) + "..." : "Data Not Avaialble"}
                       description={article.description?article.description.slice(0,60) + "...": "Data Not Avaialble"}
                       image={article.urlToImage?article.urlToImage: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"}
@@ -47,9 +57,14 @@ export default class Blog extends Component {
                       href={article.url}
                       />
                       )
-                })}              
+                })}               */}
             </div>
           </div>
+          <div className="w-full p-2 flex items-center justify-center">
+                <button className="px-8 py-2 text-lg text-white bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600 hover:scale-105 hover:-translate-y-1 transform transition-all">
+                  Load More
+                </button>
+            </div>
         </div>
       </div>
     );
